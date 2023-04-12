779d902dc8097d81010275d6c8707bc5e82a6279
Priyanka Chourey <priyanka.choure10@gmail.com>
2023-04-12 13:24:50 +0530

    connected to database

---
+++ backend/src/config/dbconfig.js
@@ -0,0 +1,27 @@
+const dotenv = require("dotenv");
+const Joi = require("joi");
+const path = require("path");
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid("production", "development", "test")
+      .required(),
+    PORT: Joi.number().default(3000),
+    MONGODB_URL: Joi.string().required().description("Mongo DB url"),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
+module.exports = {
+  mongoose: {
+    url: envVars.MONGODB_URL + (envVars.NODE_ENV === "test" ? "-test" : ""),
+    options: {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,17 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+const dbConfig = require("./config/dbconfig");
+const config = require("./config/config");
 
-server = app.listen(8082, () => {
-  console.log("listing to port", 8082);
-});
+let server;
+
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
+mongoose
+  .connect(dbConfig.mongoose.url, dbConfig.mongoose.options)
+  .then(() => {
+    console.log("Connected to Mongodb");
+    server = app.listen(config.port, () => {
+      console.log("listing to port", config.port);
+    });
+  })
+  .catch();
